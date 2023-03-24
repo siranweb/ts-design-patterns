bcab87237d86dffa21c6b4a55b6225d1ba05d5c2
SiRanWeb <kirillg.web@gmail.com>
2023-03-24 12:51:47 +0300

    add decorator link

--- patterns/structural/4-decorator/index.ts
+++ patterns/structural/4-decorator/index.ts
@@ -1,6 +1,8 @@
 import { Notifier } from './notifier';
 import { EmailDecorator, SlackDecorator } from './decorators';
 
+// https://refactoring.guru/design-patterns/decorator
+
 let notifier = new Notifier();
 notifier = new EmailDecorator(notifier);
 notifier = new SlackDecorator(notifier);
